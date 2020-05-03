6d95b03f9465c7b39486338abc9de34dacad35dd
pradeepkumarbatta <batta.pradeepkumar@gmail.com>
2020-05-03 19:55:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -46,3 +46,6 @@ app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+callsalesforce(){
+alert();
+}
